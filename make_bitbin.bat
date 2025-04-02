7e76fbb739c48b5a3b2c90a0afcadd919634889e
Wenstrand <douglas.wenstrand@jhuapl.edu>
2025-04-02 18:26:23 -0400

    better description in make_bitbin.bat

--- make_bitbin.bat
+++ make_bitbin.bat
@@ -1,5 +1,6 @@
 : this file assumes you have a bitfile in the impl_1 directory, and turns it into a bit.bin file
-: for fpgautil
+: for fpgautil.  Note that you don't need to run this batch file if you've already run "make_project.bat"
+: the use for this file is if you have generated a .bit file from the GUI and wanted to turn it into a .bit.bin
 call c:\Xilinx\Vivado\2024.2\settings64.bat
 call bootgen -image doug.bif -arch zynq -process_bitstream bin -w on
 
